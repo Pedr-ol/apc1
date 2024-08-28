c4de91c527847a2fc67e2e05a51361ad30f89a53
Pedr-ol <pedrooliver1012@gmail.com>
2024-08-28 12:57:48 +0000

    Adicionando arquivos da pasta pratica03

--- praticas/pratica02/imprima_nota_legal.c
+++ praticas/pratica02/imprima_nota_legal.c
@@ -4,12 +4,12 @@ int main() {
     printf("%s\n", "==================================");
     printf("%s\n", "           NOTA LEGAL         ");
     printf("%s\n", "===================================");
-    printf("%-13s %7s %13s\n", "Produto", "Qtd", "Valor Unit");
-    printf("%-13s %7.3i\t %10.2f\n", "Camiseta", 002, 39.99);
-    printf("%-13s %8.3i\t %10.2f\n", "Calça", 001, 89.90);
-    printf("%-13s %7.3i\t %10.2f\n", "Meia Social", 003, 19.99);
+    printf("%-15s %9s %13s\n", "Produto", "Qtd", "Valor Unit");
+    printf("%-15s %9.3i %10.2f\n", "Camiseta", 002, 39.99);
+    printf("%-15s %10.3i %10.2f\n", "Calça", 001, 89.90);
+    printf("%-15s %9.3i %10.2f\n", "Meia Social", 003, 19.99);
     printf("%s\n", "===================================");
-    printf("%s %23.2f\n", "Total:", 229.85);
+    printf("%s %29.2f\n", "Total:", 229.85);
   
   return 0;
 }
---
+++ praticas/pratica03/declara_constantes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+  const float PI = 3.141593;
+  const char  SEXO_MASCULINO ='M';
+  //PI = 3.14;
+  printf("Valor de PI: %f\n", PI);
+  printf("Sexo Masculino: %c\n", SEXO_MASCULINO);
+return 0;
+}
